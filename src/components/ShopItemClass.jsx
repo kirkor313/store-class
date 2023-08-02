a065619ba6e22f0eb8541739b5eb017623591701
kirkor313 <kirkor313@mail.ru>
2023-08-02 20:10:03 +0300

    feat: add component ShopItemClass

---
+++ src/components/ShopItemClass.jsx
@@ -0,0 +1,23 @@
+import { Component } from 'react'
+
+export default class ShopItemClass extends Component {
+  render(props) {
+    const {item} = this.props;
+    return (
+      <div class="main-content">
+      <h2>{item.brand}</h2>
+      <h1>{item.title}</h1>
+      <h3>{item.description}</h3>
+      <div class="description">{item.descriptionFull}</div>
+      <div class="highlight-window mobile">
+        <div class="highlight-overlay"></div>
+      </div>
+      <div class="divider"></div>
+      <div class="purchase-info">
+        <div class="price">{item.currency}{item.price}</div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+    )
+  }
+}
